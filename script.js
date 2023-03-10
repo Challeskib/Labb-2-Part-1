66c0c56fcfdfc6d181ecaaed3ac6b10e36ce4ac8
Charlie Skibsted <charlieskibsted@gmail.com>
2023-03-10 14:48:26 +0100

    Labb 2 part 1 Webbutveckling

---
+++ script.js
@@ -0,0 +1,34 @@
+const button = document.getElementById("secretButton");
+button.addEventListener("click", function () {
+  document.body.style.background = "pink";
+  console.log("You clicked the button!");
+});
+
+const button2 = document.getElementById("secretButton2");
+button2.addEventListener("click", function () {
+  document.body.style.background = "white";
+  console.log("You clicked the button!");
+});
+
+const elit = [1, 3, 3, 7];
+let rightAnswer = [];
+let counter = 0;
+
+const kombination = document.getElementById("secretKey");
+
+kombination.addEventListener("keypress", function (event) {
+  if (event.key == elit[counter]) {
+    rightAnswer.push(event.key);
+    console.log(rightAnswer);
+    counter++;
+  } else {
+    counter = 0;
+    rightAnswer.length = 0;
+  }
+  if (counter == 4) {
+    console.log("GZ");
+
+    counter = 0;
+    rightAnswer.length = 0;
+  }
+});
